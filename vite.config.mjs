6f4ce8a0fbeca03a64c0158a85cef6d733a56046
vimtor <vn4varro@gmail.com>
2024-03-11 23:47:27 +0100

    Make it Vercel ready

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,13 @@
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
+import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
     remix({
-      ignoredRouteFiles: ["**/.*"],
+      presets: [vercelPreset()]
     }),
-  ],
+    tsconfigPaths()
+  ]
 });

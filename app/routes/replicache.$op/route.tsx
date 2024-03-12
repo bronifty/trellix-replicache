b266ac9d59ec4acfd101d023913bf8a8f48e79a4
vimtor <vn4varro@gmail.com>
2024-03-12 16:05:24 +0100

    Comment about backend strategy

--- app/routes/replicache.$op/route.tsx
+++ app/routes/replicache.$op/route.tsx
@@ -2,15 +2,9 @@ import { type LoaderFunctionArgs } from "@remix-run/node";
 import { handleReplicachePull } from "./pull";
 import { handleReplicachePush } from "./push";
 
+// I'm using the Reset View backend strategy for Replicache: https://doc.replicache.dev/strategies/reset
+// It is not the most scalable strategy, but it works well for the purposes of this demo.
 export async function action({ request, params }: LoaderFunctionArgs) {
-  if (params.op === "pull") {
-    return handleReplicachePull(request);
-  } else if (params.op === "push") {
-    return handleReplicachePush(request);
-  }
-}
-
-export async function loader({ request, params }: LoaderFunctionArgs) {
   if (params.op === "pull") {
     return handleReplicachePull(request);
   } else if (params.op === "push") {
